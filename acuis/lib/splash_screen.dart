1ca2cbeffab25ec6ba646b0cfc5f3ef121d195a3
maskeen <maskeensingh@protonmail.com>
2026-03-31 21:28:36 +0530

    fix c import

--- acuis/lib/splash_screen.dart
+++ acuis/lib/splash_screen.dart
@@ -1,4 +1,4 @@
-cimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'main.dart';
